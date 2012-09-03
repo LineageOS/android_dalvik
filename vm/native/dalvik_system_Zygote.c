72ac2c6265727ebe8f371f0f7d90dec0202fedce
xkonni <konstantin.koslowski@gmail.com>
2012-09-02 16:06:07 -1000

    Added missing <include sys/resource.h> for gcc-4.7.1 Pulled from http://review.cyanogenmod.com/#/c/18580/2
    with https://github.com/CyanogenMod/hp-kernel-tenderloin/blob/36523048fc498076ad31e8357b12a74ac9622315/arch/arm/mach-msm/scm-io.c#L24 for more of a future proof
    
    Change-Id: I89bed8c2942eae14e5a14f5e032d993a161f5c27

--- vm/native/dalvik_system_Zygote.c
+++ vm/native/dalvik_system_Zygote.c
@@ -21,6 +21,13 @@
 #include "native/InternalNativePriv.h"
 
 #include <signal.h>
+#if defined(__GNUC__) && \
+	defined(__GNUC_MINOR__) && \
+	defined(__GNUC_PATCHLEVEL__) && \
+	((__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) \
+		> 40501
+#include <sys/resource.h>
+#endif
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <grp.h>
