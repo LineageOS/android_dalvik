fd64a144d93f3c9bd5ea02eb54a446d78b2e8e0b
Rocky Zhang <zhangyan.hit@gmail.com>
2012-11-29 08:02:29 +0800

    dalvik: remove duplicated call to dvmJitCalleeSave()
    Change-Id: Ib7b5f637a22fd5ba60bdb45cd34ac8c1781c7bd3
    Author: Rocky Zhang <zhangyan.hit@gmail.com>
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- vm/interp/Interp.cpp
+++ vm/interp/Interp.cpp
@@ -1926,7 +1926,6 @@ void dvmInterpret(Thread* self, const Method* method, JValue* pResult)
 #endif
     self->debugIsMethodEntry = true;
 #if defined(WITH_JIT)
-    dvmJitCalleeSave(calleeSave);
     /* Initialize the state to kJitNot */
     self->jitState = kJitNot;
 #endif
