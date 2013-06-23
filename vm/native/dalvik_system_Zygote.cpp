d1a1f37dce89a7297dc6aaaf8b6ffeb0c631de40
coudu <coudu@wanadoo.fr>
2013-06-23 02:01:21 +0200

    Fix for GCC 4.8.x
        GCC 4.8.x needs <include sys/resource.h>
        This expands gcc version check to include sys/resource.h for
        all 4.8.x versions
    
    Change-Id: Ic1b52ba665ce79bfb0444c17386e6caf7450a858

--- vm/native/dalvik_system_Zygote.cpp
+++ vm/native/dalvik_system_Zygote.cpp
@@ -21,7 +21,7 @@
 #include "native/InternalNativePriv.h"
 
 #include <signal.h>
-#if (__GNUC__ == 4 && __GNUC_MINOR__ == 7)
+#if (__GNUC__ == 4 && (__GNUC_MINOR__ == 7 || __GNUC_MINOR__ == 8))
 #include <sys/resource.h>
 #endif
 #include <sys/types.h>
